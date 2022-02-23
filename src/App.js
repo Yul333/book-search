cd61f72c042a7d2d42df1cc48c85dd491444cac6
Yulia Kitan <yuliakitan@gmail.com>
2022-02-23 16:47:43 +0200

    book

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  async function makeGetRequest(search) {
+    let res = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyBobZEVkpE7_20Bxux76ICQaR8N-1QPaWg`
+    )
+
+    let data = res.data
+    console.log(data.items)
+    setSearchResults(data.items)
+  }
+
+  const [searchField, setSearchField] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+
+  const handleChange = (event) => {
+    const book = event.target.value
+    setSearchField(book)
+    console.log(book)
+
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(searchField)
+    makeGetRequest(searchField)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <h1>Book Search</h1>
       </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Search'
+          value={searchField}
+          onChange={handleChange}
+        />
+        <button type='submit'>Find me a Book!</button>
+      </form>
+      {/* <ul> */}
+      <div className="cards-container">
+        {searchResults.map(book => (
+      <div className="card">
+        <div className="card__title">{book.volumeInfo.title} </div>
+        
+        {/* <div className="card__body"> */}
+          <a target='blank' href={book.volumeInfo.previewLink}>
+            <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
+          </a>
+        <div className="card__publish">{book.volumeInfo.publishedDate} </div>
+        <div className="card__author">{book.volumeInfo.authors} </div>
+        <div className="card__info">{book.searchInfo.textSnippet} </div>
+          </div>
+        //  </div>
+          ))}
+          </div> 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
